f04abda020d96473e42227095862cbf37b565e77
alertzero <faceroxz@gmail.com>
2022-03-12 20:16:47 +0700

    revised CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
   resources :inboxes
   root 'static_pages#landing_page'
   get 'pricing', to: 'static_pages#pricing'
