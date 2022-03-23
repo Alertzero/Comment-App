5c4129dd8d26fe0e0e102d15ec50bfa0e17452bb
alertzero <faceroxz@gmail.com>
2022-03-23 14:38:01 +0700

    added githubomni

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,20 +13,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
 
     def github
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env['omniauth.auth'])
-
       if @user.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Github'
         sign_in_and_redirect @user, event: :authentication
       else
-        session['devise.auth_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+        session['devise.auth_data'] = request.env['omniauth.auth'].except(:extra)
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
       end
-  end
+    end
 
-  def failure 
-    redirecto_to rootpath, alert: "Failure Please try again" 
-  end
+    def failure
+      redirect_to root_path, alert: 'Failure. Please try again'
+    end
 
+
+  
   end
