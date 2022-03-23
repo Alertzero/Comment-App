c223a114b800a3a17ce8184b157060664754aefe
alertzero <faceroxz@gmail.com>
2022-03-23 15:39:59 +0700

    added githubomni

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,2 @@
-OmniAuth.config.allowed_request_methods = [:get, :post]
-OmniAuth.config.full_host = Rails.env.production? ? 'https://secret-eyrie-95727.herokuapp.com' : 'http://localhost:3000'
+
 OmniAuth.config.allowed_request_methods = %i[get]
