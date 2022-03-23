49a904586362459eb98b83190869099bdedaa593
alertzero <faceroxz@gmail.com>
2022-03-23 14:51:20 +0700

    added githubomni

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :github]
   has_many :inboxes, dependent: :destroy
   has_many :messages, through: :inboxes
+  
   acts_as_voter
   
   def self.from_omniauth(access_token)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,2 +1,3 @@
 OmniAuth.config.allowed_request_methods = [:get, :post]
-OmniAuth.config.full_host = "https://secret-eyrie-95727.herokuapp.com/"
+OmniAuth.config.full_host = Rails.env.production? ? 'https://secret-eyrie-95727.herokuapp.com' : 'http://localhost:3000'
+OmniAuth.config.allowed_request_methods = %i[get]
